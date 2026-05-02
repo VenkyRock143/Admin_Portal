14d02078024566cc9db081ce06393b35e79b32ec
VenkyRock143 <venky.balusani1@gmail.com>
2026-05-02 15:28:06 +0530

    step2: serve frontend UI

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask;
+from flask import Flask, send_from_directory
+from pathlib import Path
 
-app = Flask(__name__)
+BASE_DIR = Path(__file__).parent
+
+app = Flask(
+    __name__,
+    static_folder=str(BASE_DIR / "sky"),
+    static_url_path=""
+)
 
 @app.route("/")
-def home():
-    
-    return "Flask is running!"
-
+def server_index():
+     return send_from_directory(app.static_folder, "admin.html")
+ 
 if __name__ == "__main__":
     app.run(debug = True)
